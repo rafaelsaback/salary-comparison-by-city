a39a021704070dcb4d1f77f42d91d581bee152e0
Rafael Saback <rafaelsaback@gmail.com>
2019-02-11 20:29:10 +0100

    Set up eslint based on Airbnb guide

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,10 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ]
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  "extends": "airbnb",
+  "rules": {
+    "import/no-unresolved": "off",
+  },
+  "env": {
+    "browser": true,
+    "node": true,
+  },
 };
