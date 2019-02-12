e31b55930314a85a22c6db8bed864c004c621533
Rafael Saback <rafaelsaback@gmail.com>
2019-02-12 10:18:15 +0100

    Correct indentation in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,44 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-  entry: ['./src/index.js', './src/styles/main.scss'],
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, './dist')
-    },
+  mode: 'development',
+  entry: ['./src/index.jsx', './src/styles/main.scss'],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist'),
+  },
 
-    devtool: 'source-map',
-    devServer: {
-        inline: true,
-        contentBase: path.join(__dirname, 'dist'),
-        port: 4000,
-        open: true
-    },
+  devtool: 'source-map',
+  devServer: {
+    inline: true,
+    contentBase: path.join(__dirname, 'dist'),
+    port: 4000,
+    open: true,
+  },
 
-    resolve: {
-      extensions: ['.js', '.jsx', '.json', '.scss', '.css'],
-        modules: [
-            path.resolve(__dirname, 'src'),
-            'node_modules'
-        ]
-    },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.scss', '.css'],
+    modules: [
+      path.resolve(__dirname, 'src'),
+      'node_modules',
+    ],
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /(node_modules)/,
-                use: ['babel-loader']
-            },
-            {
-                test: /\.(s*)css$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-            },
-            {
-                test: /\.(pdf|jpg|jpeg|png|gif|svg|ico)$/,
-                use: ['url-loader']
-            },
-        ]
-    },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(s*)css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(pdf|jpg|jpeg|png|gif|svg|ico)$/,
+        use: ['url-loader'],
+      },
+    ],
+  },
 };
