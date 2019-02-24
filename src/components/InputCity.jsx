fd017a72e930680b8be046df6c362716cb75b693
Rafael Saback <rafaelsaback@gmail.com>
2019-02-24 17:01:25 +0100

    Improve performance of InputCity
    The Options data was first filtered by the first letter that the user
    writes. In that way, the amount of data to be exhibitted in the dropdown
    is way smaller.

--- src/components/InputCity.jsx
+++ src/components/InputCity.jsx
@@ -4,44 +4,65 @@ import { Select, Form } from 'antd';
 
 const { Option } = Select;
 
-const InputCity = props => {
-  const {
-    cities,
-    isError,
-    fieldID,
-    errorMessage,
-    getFieldDecorator,
-    onChange,
-  } = props;
-  return (
-    <Form.Item
-      label={fieldID === 'srcCity' ? 'Source City' : 'Target City'}
-      validateStatus={isError ? 'error' : ''}
-      help={isError || ''}
-    >
-      {getFieldDecorator(fieldID, {
-        rules: [{ required: true, message: errorMessage }],
-      })(
-        <Select
-          showSearch
-          placeholder="Select a city"
-          optionFilterProp="children"
-          onChange={onChange}
-          filterOption={(input, option) =>
-            option.props.children.toLowerCase().indexOf(input.toLowerCase()) >=
-            0
-          }
-        >
-          {cities.map(city => (
-            <Option key={city.id} value={city.name}>
-              {city.name}
-            </Option>
-          ))}
-        </Select>
-      )}
-    </Form.Item>
-  );
-};
+class InputCity extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstLetter: 'A',
+    };
+  }
+
+  onSearch = value => {
+    if (value) {
+      this.setState({ firstLetter: value[0].toUpperCase() });
+    } else {
+      this.setState({ firstLetter: 'A' });
+    }
+  };
+
+  render() {
+    const {
+      cities,
+      isError,
+      fieldID,
+      errorMessage,
+      getFieldDecorator,
+      onChange,
+    } = this.props;
+    const { firstLetter } = this.state;
+    const filteredCities = cities.filter(city => city.name[0] === firstLetter);
+    return (
+      <Form.Item
+        label={fieldID === 'srcCity' ? 'Source City' : 'Target City'}
+        validateStatus={isError ? 'error' : ''}
+        help={isError || ''}
+      >
+        {getFieldDecorator(fieldID, {
+          rules: [{ required: true, message: errorMessage }],
+        })(
+          <Select
+            showSearch
+            placeholder="Select a city"
+            optionFilterProp="children"
+            onSearch={this.onSearch}
+            onChange={onChange}
+            filterOption={(input, option) =>
+              option.props.children
+                .toLowerCase()
+                .indexOf(input.toLowerCase()) >= 0
+            }
+          >
+            {filteredCities.map(city => (
+              <Option key={city.id} value={city.name}>
+                {city.name}
+              </Option>
+            ))}
+          </Select>
+        )}
+      </Form.Item>
+    );
+  }
+}
 
 InputCity.propTypes = {
   cities: PropTypes.arrayOf(
